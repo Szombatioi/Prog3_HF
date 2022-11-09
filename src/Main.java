6f26c2f4f93bb933e601665eecf4f7375b3cab5e
Szombatioi <o.szombati@gmail.com>
2022-11-09 16:42:58 +0100

    First upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import java.awt.Toolkit;
+
+public class Main {
+	public static void main(String[] args) {
+		Window window = new Window("MineSweeper 2.0", 600, 400, Toolkit.getDefaultToolkit().getImage("images/icon.png"));
+	}
+}
---
+++ src/Window.java
@@ -0,0 +1,34 @@
+import javax.swing.*; //TODO csak a szükségeseket!
+import java.awt.*; //TODO csak a szükségeseket!
+
+public class Window extends JFrame{
+	private JPanel Menu;
+	
+	
+	
+	public Window(String title, int w, int h) {
+		setTitle(title);
+		setSize(new Dimension(w,h));
+		setMinimumSize(new Dimension(w,h));
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		requestFocus();
+		setVisible(true);
+		
+		setLayout(new BorderLayout());
+		Menu = new JPanel();
+		Menu.add(new JLabel("MineSweeper 2.0"));
+		
+		add(Menu);
+		
+		
+		
+		
+		pack();
+	}
+	
+	public Window(String title, int w, int h, Image img) {
+		this(title, w, h);
+		if(img!=null) setIconImage(img);
+	}
+}
