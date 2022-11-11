66834c91d9ed0cf13412f174342d7eb1014b6e33
Szombatioi <62300631+Szombatioi@users.noreply.github.com>
2022-11-11 22:49:55 +0100

    images.load to Images.load

--- src/Game/Main.java
+++ src/Game/Main.java
@@ -8,15 +8,16 @@ public class Main {
 	public static void main(String[] args) {
 		boolean working = true;
 		
-		Images images = new Images();
+		//Images images = new Images();
 		try {
-			images.loadImages();
+			Images.loadImages();
 		}catch(Exception e) {
 			working = false;
 			ErrorWindow err = new ErrorWindow("Error", Images.error);
 		}
 		Window window;
-		if(working) window = new Window("MineSweeper 2.0", 600, 400, Toolkit.getDefaultToolkit().getImage("images/icon.png"));
+		//if(working) window = new Window("MineSweeper 2.0", 600, 400, Toolkit.getDefaultToolkit().getImage("images/icon.png"));
+		if(working) window = new Window("MineSweeper 2.0", 600, 400, Images.icon);
 		
 	}
 }
